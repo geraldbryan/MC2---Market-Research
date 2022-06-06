aeb387696e5f2f302b7f846f36bfe81d6a38c006
geraldbryan <geraldbryan9914@gmail.com>
2022-06-06 22:22:44 +0700

    Initial Commit

---
+++ MC2 - Market Research/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC2 - Market Research
+//
+//  Created by Gerald Bryan on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
