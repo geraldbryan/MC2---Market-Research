a4c2450728132a8c721e4a153b32561bd9946dc7
geraldbryan <geraldbryan9914@gmail.com>
2022-06-14 00:00:56 +0700

    add carousel (image slideshow)

---
+++ MC2 - Market Research/marketResearchStep.swift
@@ -0,0 +1,40 @@
+//
+//  marketResearchStep.swift
+//  MC2 - Market Research
+//
+//  Created by Gerald Bryan on 13/06/22.
+//
+
+//import Foundation
+//import UIKit
+//
+//struct researchStep {
+//    var stepName: String?
+//    var stepWhy: String?
+//    var stepWhat: String?
+//    var resourceTools: String?
+//    var imageVector: UIImage?
+//    var imageColor: UIImage?
+//}
+//
+//func initData() {
+//
+//    var researchPage = [researchStep]()
+//
+//    // Append swot analysis into research page data
+//    researchPage.append(researchStep(stepName: "SWOT Analysis", stepWhy: "SWOT Analysis can help you to challenge risky assumptions and to uncover dangerous blindspots about your organization's performance. It will help you to develop exactly the right strategy for any situation.", stepWhat: "SWOT analysis is a technique for assessing the performance, competition, risk, and potential of a business, as well as part of a business such as a product line or division, an industry, or other entity.", resourceTools: "You can explore the resource and try to write your SWOT Analysis.", imageVector: <#T##UIImage?#>, imageColor: <#T##UIImage?#>))
+//
+//    // Append target market analysis into research page data
+//    researchPage.append(researchStep(stepName: "Target Market Analysis", stepWhy: "Target market analysis will help businesses to stay competitive in the marketplace and achieve maximum profitability on niche market segments.", stepWhat: "Target market analysis is an assessment of how your product or service fits into a specific market and where it will gain the most traction with customers.", resourceTools: "You can explore and try at least one of these tools to analyze your target market.", imageVector: <#T##UIImage?#>, imageColor: <#T##UIImage?#>))
+//
+//    // Append competitive analysis into research page data
+//    researchPage.append(researchStep(stepName: "Competitive Analysis", stepWhy: "A competitive analysis is a critical part of your company marketing plan. With this evaluation, you can establish what makes your product or service unique--and therefore what attributes you play up in order to attract your target market.", stepWhat: "Identifying your competitors and evaluating their strategies to determine their strengths and weaknesses relative to those of your own product or service.", resourceTools: "You can explore and try at least one of these tools to analyze your competitor.", imageVector: <#T##UIImage?#>, imageColor: <#T##UIImage?#>))
+//
+//    // Append indetify trend into research page data
+//    researchPage.append(researchStep(stepName: "Identify Trend", stepWhy: "Identify trends will give you a wider variety of things to work with- therefore, you will be able to experiment with more things and use them to generate new ideas.", stepWhat: "Identifying a market trend analysis uncovers trends in both an industry and within a target market — the group of customers most likely to purchase from a company.", resourceTools: "You can explore and try at least one of these tools to analyze the market trends.", imageVector: <#T##UIImage?#>, imageColor: <#T##UIImage?#>))
+//
+//    researchPage.append(researchStep(stepName: "Marketing Plan", stepWhy: "Marketing plan will help determine on how best to reach them and generate a positive return of investment from the total of marketing strategies spend. ", stepWhat: "Marketing plan is an advertising strategy that a business will implement to sell its product or service.", resourceTools: "You can explore the resource and try to write your SWOT Analysis.", imageVector: <#T##UIImage?#>, imageColor: <#T##UIImage?#>))
+//
+//}
+
+
